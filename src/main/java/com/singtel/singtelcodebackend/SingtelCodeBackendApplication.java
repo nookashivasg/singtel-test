4bb2f13ccc6e90f4d82d24f024eb04b0406e9d40
shiva nooka <shivaexpjava@gmail.com>
2021-08-08 11:17:51 +0800

    Initial Commit

---
+++ src/main/java/com/singtel/singtelcodebackend/SingtelCodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.singtel.singtelcodebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingtelCodeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingtelCodeBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/singtel/singtelcodebackend/Solution.java
@@ -0,0 +1,8 @@
+package com.singtel.singtelcodebackend;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
